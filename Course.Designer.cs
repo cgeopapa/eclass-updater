0ce66e726ccc06216616194119a22ea99f947627
qaz wsx <cgeocodgod@gmail.com>
2019-11-17 15:43:44 +0200

    New forms

---
+++ Course.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace eclass_updater
+{
+    partial class Course
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.update = new System.Windows.Forms.CheckBox();
+            this.name = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // update
+            // 
+            this.update.AutoSize = true;
+            this.update.Location = new System.Drawing.Point(4, 9);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(15, 14);
+            this.update.TabIndex = 0;
+            this.update.UseVisualStyleBackColor = true;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(25, 9);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(166, 13);
+            this.name.TabIndex = 1;
+            this.name.Text = "Huge Course Name that is long af";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "F:/Path/To/Download/Folder";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(347, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Course
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.update);
+            this.MinimumSize = new System.Drawing.Size(425, 0);
+            this.Name = "Course";
+            this.Size = new System.Drawing.Size(425, 33);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox update;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
